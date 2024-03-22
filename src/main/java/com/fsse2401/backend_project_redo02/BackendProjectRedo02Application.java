80f38b4a2fa15039821dc9a252895c9b571d2bb8
chloros <chloros.lin@gmail.com>
2024-03-22 15:42:54 +0800

    upload files

---
+++ src/main/java/com/fsse2401/backend_project_redo02/BackendProjectRedo02Application.java
@@ -0,0 +1,13 @@
+package com.fsse2401.backend_project_redo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectRedo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectRedo02Application.class, args);
+	}
+
+}
